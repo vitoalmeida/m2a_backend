60dc0230703aab74ffedcb08ea4fa64cb9344828
unknown <vitoralmeida.work@gmail.com>
2022-09-04 15:02:38 -0300

    Setup

--- api/settings.py
+++ api/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#3@$-ii#kje0zs!fhu#8@sur-puhbt-yzcy-!9kcjr892-(+43'
+SECRET_KEY = '82cfb734babc3e059f83fee76c96bb739b9453b2493f8ac8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
